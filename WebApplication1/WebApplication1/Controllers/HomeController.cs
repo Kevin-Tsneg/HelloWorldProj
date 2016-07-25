359bc5e28fa568442158954facf3ebd78d7596a7
Kevin-Tsneg <rhca168@gmail.com>
2016-07-25 17:47:06 +0800

    Update HomeController.cs

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            string name = "yuchang tseng!!123456";
+            string name = "yuchang tseng!!123456789";
             return View();
         }
 
